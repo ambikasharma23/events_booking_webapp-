126667b8aafa81b1f8ce6eba51a88322c4d63855
Priti Thakur <priti@eazydiner.com>
2024-05-16 15:15:03 +0530

    Get api for tickets

--- Backend/Routes/ticketRoute.js
+++ Backend/Routes/ticketRoute.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const ticketController = require('../Controllers/ticketcontroller');
+const ticketController = require("../Controllers/ticketcontroller");
 
 // Create a ticket for a particular event
-router.post('/', ticketController.createTicket);
+router.post("/ticket/create", ticketController.createTicket);
 
 // Update a ticket by ID
-router.put('/:id', ticketController.updateTicket);
+router.put("/ticket/:id", ticketController.updateTicket);
 
 // Delete a ticket by ID
-router.delete('/:id', ticketController.deleteTicket);
+router.delete("/ticket/:id", ticketController.deleteTicket);
 
 module.exports = router;
--- Backend/index.js
+++ Backend/index.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const app = express();
 const db = require("./models");
-const router = require("./Routes/eventRoute");
-app.use(express.json());
-app.use("/", router);
+
 const PORT = process.env.PORT || 3001;
 
-db.sequelize
-  .sync()
-  .then(() => {
+app.use(express.json());
+app.use("/", require("./Routes/eventRoute"));
+app.use("/", require("./Routes/ticketRoute"));
+
+
+db.sequelize.sync().then(() => {
     app.listen(PORT, () => {
       console.log("Server is running on port", PORT);
     });
-  })
-  .catch((error) => {
+  }).catch((error) => {
     console.error("Error starting server:", error);
-  });
+});
