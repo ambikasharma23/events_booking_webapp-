186bace33d0a348e738b65319b9e07b4f347379c
Priti Thakur <priti@eazydiner.com>
2024-06-19 15:45:17 +0530

    Category design

--- Frontend/event_frontend/app/page.tsx
+++ Frontend/event_frontend/app/page.tsx
@@ -1,9 +1,10 @@
 import HomePage from '../app/components/home/page';
-
+import Category from '../app/components/home/category';
 export default function Home() {
   return (
     <main>
       <HomePage />
+      <Category />
     </main>
   );
 }
