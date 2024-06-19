c52843cfb8d3549f258cde66d83b6d13f2f6163a
Priti Thakur <priti@eazydiner.com>
2024-06-19 16:25:18 +0530

    Category fetch from backend

--- Frontend/event_frontend/app/components/home/category.tsx
+++ Frontend/event_frontend/app/components/home/category.tsx
@@ -1,62 +1,50 @@
 "use client";
+import React, { useEffect, useState } from 'react';
 
-import React from 'react';
+export default function Category() {
+    const [categories, setCategories] = useState([]);
 
-const Category = () => {
-  return (<>
-      <h1 className="text-white mx-10">What are you looking at ?</h1>
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                let response = await fetch("http://localhost:3001/getCategory");
+                if (!response.ok) {
+                    throw new Error('Failed to fetch data');
+                }
+                let data = await response.json();
+                console.log(data);
+                setCategories(data); 
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
 
-    <section className="text-gray-100 body-font">
-      <div className="container mx-auto">
-        <div className="grid grid-cols-4 md:grid-cols-7 ">
-          <div className="p-1 md:p-4 w-full ">
-            <div className="h-full border-2 border-gray-200 border-opacity-10  rounded-lg overflow-hidden">
-              <img className="h-10 md:h-20 w-full object-cover object-center" src="https://shorturl.at/fY6OZ" alt="blog" />
-              <h4 className="title-font text-sm font-medium text-gray-900 text-white text-center">Music</h4>
-            </div>
-          </div>
-          <div className="p-1 md:p-4 w-full">
-            <div className="h-full border-2 border-gray-200 border-opacity-10 rounded-lg overflow-hidden">
-              <img className="h-10 md:h-20 w-full object-cover object-center" src="https://shorturl.at/m0GoV" alt="blog" />
-              <h4 className="title-font text-sm font-medium text-gray-900  text-white	text-center ">Dance</h4>
-            </div>
-          </div>
-          <div className="p-1 md:p-4 w-full">
-            <div className="h-full border-2 border-gray-200 border-opacity-10 rounded-lg overflow-hidden">
-              <img className="h-10 md:h-20 w-full object-cover object-center" src="https://shorturl.at/uqJcJ" alt="blog" />
-              <h4 className="title-font text-sm font-medium text-gray-900 text-white text-center">Food</h4>
-            </div>
-          </div>
-          <div className="p-1 md:p-4 w-full">
-            <div className="h-full border-2 border-gray-200 border-opacity-10 rounded-lg overflow-hidden">
-              <img className="h-10 md:h-20 w-full object-cover object-center" src="https://shorturl.at/cRlL3" alt="blog" />
-              <h4 className="title-font text-sm font-medium text-gray-900 text-white text-center">Night</h4>
-            </div>
-          </div>
-          <div className="p-1 md:p-4 w-full">
-            <div className="h-full border-2 border-gray-200 border-opacity-10 rounded-lg overflow-hidden">
-              <img className="h-10 md:h-20 w-full object-cover object-center" src="https://shorturl.at/WBXbW" alt="blog" />
-              <h4 className="title-font text-sm font-medium text-gray-900 text-white text-center">Cultural</h4>
-            </div>
-          </div>
-          <div className="p-1 md:p-4 w-full">
-            <div className="h-full border-2 border-gray-200 border-opacity-10 rounded-lg overflow-hidden">
-              <img className="h-10 md:h-20 w-full object-cover object-center" src="https://shorturl.at/Zcha7" alt="blog" />
-              <h4 className="title-font text-sm font-medium text-gray-900 text-white text-center">DJ</h4>
-            </div>
-          </div>
-          <div className="p-1 md:p-4 w-full">
-            <div className="h-full border-2 border-gray-200 border-opacity-10 rounded-lg overflow-hidden">
-              <img className="h-10 md:h-20 w-full object-cover object-center" src="https://shorturl.at/CW83T" alt="blog" />
-              <h4 className="title-font text-sm font-medium text-gray-900 text-white text-center">Live</h4>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-    
-  </>
-  );
+        fetchData(); 
+    }, []); 
+
+    return (
+        <>
+            <h1 className="text-white mx-20 font-bold">What are you looking at?</h1>
+
+            <section className="text-gray-100 body-font">
+                <div className="container mx-auto">
+                    <div className="grid grid-cols-4 md:grid-cols-8 gap-4">
+                        {categories.map((categories) => (
+                            <div className="p-1 md:p-4 w-full" key={categories.id}>
+                                <div className="h-full border-2 border-gray-200 border-opacity-10 rounded-lg overflow-hidden">
+                                    <img
+                                        className="h-10 md:h-20 w-full object-cover object-center"
+                                        src={categories.icon}
+                                    />
+                                    <h4 className="title-font text-sm font-medium text-gray-900 text-white text-center">
+                                        {categories.name}
+                                    </h4>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </section>
+        </>
+    );
 }
-
-export default Category;
