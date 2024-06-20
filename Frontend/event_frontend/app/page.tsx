b583f052d62f6478b60b9f7d846059d8c6e2d733
Priti Thakur <priti@eazydiner.com>
2024-06-20 12:02:18 +0530

    Listing categories in the home page

--- Frontend/event_frontend/app/page.tsx
+++ Frontend/event_frontend/app/page.tsx
@@ -1,12 +1,16 @@
 import HomePage from '../app/components/home/page';
 import Category from '../app/components/home/category';
 import Music from './components/home/music';
+import Comedy from './components/home/comedy';
+import Dance from './components/home/dance';
 export default function Home() {
   return (
     <main>
       <HomePage />
       <Category />
       <Music />
+      <Comedy />
+      <Dance />
     </main>
   );
 }
