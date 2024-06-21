7409dec02ed64669540cb87b7fec695d9af0b75f
Priti Thakur <priti@eazydiner.com>
2024-06-21 12:04:05 +0530

    Carousel in Home page

--- Frontend/event_frontend/app/components/home/music.tsx
+++ Frontend/event_frontend/app/components/home/music.tsx
@@ -1,10 +1,15 @@
 "use client";
 import React, { useEffect, useState } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 interface MusicCat {
   id: number;
   event_image: string;
   event_name: string;
 }
+
 export default function Music() {
   const [events, setEvents] = useState<MusicCat[]>([]);
 
@@ -28,29 +33,65 @@ export default function Music() {
     fetchData();
   }, []);
 
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
+
   return (
     <>
-      <h1 className="text-white mx-20 my-1 font-bold p-5">Music Shows</h1>
+      <h1 className="text-white mx-2 my-1 font-bold p-5">Music Shows</h1>
 
       <section className="text-gray-100 body-font">
         <div className="container mx-auto">
-          <div className="grid grid-cols-5 md:grid-cols-6 gap-4">
-            {events.map((events) => (
-              <div className="p-1 md:p-4 w-full" key={events.id}>
-                <div className="h-full border-2 border-gray-200 border-opacity-10 rounded-lg overflow-hidden">
+
+          <Slider {...settings}>
+
+            {events.map((event) => (
+              <div className="p-1 md:p-1 w-full" key={event.id}>
+                <div className="h-30 md:h-full border-2 border-gray-200 border-opacity-10 rounded-lg overflow-hidden">
                   <img
-                    className="h-10 md:h-full w-full object-cover object-center"
-                    src={events.event_image}
+                    className="h-28 md:h-40 w-full object-cover object-center"
+                    src={event.event_image}
+                    alt={event.event_name}
                   />
                 </div>
-                <h4 className="title-font text-sm font-medium text-gray-900 text-white text-center">
-                  {events.event_name}
+                <h4 className="title-font text-sm font-medium text-white text-center">
+                  {event.event_name}
                 </h4>
               </div>
             ))}
             
-          </div>
-          
+          </Slider>
         </div>
       </section>
     </>
